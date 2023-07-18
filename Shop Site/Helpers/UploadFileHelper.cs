40e015edc9f915d9bfd3dce0a3329c7e63f9f3c9
akrem <akrem@NIKO>
2023-07-18 23:07:26 +0400

    update 3.5.2

--- Shop Site/Helpers/UploadFileHelper.cs
+++ Shop Site/Helpers/UploadFileHelper.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Extensions.Hosting.Internal;
-
-namespace Shop_Site.Helpers
+﻿namespace Shop_Site.Helpers
 {
     public class UploadFileHelper
     {
         public async static Task<string> UploadFile(IFormFile file)
         {
             string currentDirectory = Directory.GetCurrentDirectory();
-            string imagePath = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+            string imagePath = $"Images/{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
             string path = Path.Combine(currentDirectory, "wwwroot", imagePath);
 
             FileStream fs = new(path, FileMode.CreateNew, FileAccess.ReadWrite);
---
+++ Shop Site/Migrations/20230718190601_mig3.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop_Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1351));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1359));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1360));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1360));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1423));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1424));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1424));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 23, 6, 1, 835, DateTimeKind.Local).AddTicks(1425));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2820));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2828));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2829));
+
+            migrationBuilder.UpdateData(
+                table: "Brands",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2830));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2905));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2905));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2906));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2023, 7, 18, 21, 38, 12, 392, DateTimeKind.Local).AddTicks(2907));
+        }
+    }
+}
