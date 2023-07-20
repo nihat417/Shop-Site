fdee390367c5c1b93d48f6de6c321522d365feef
akrem <akrem@NIKO>
2023-07-20 15:14:20 +0400

    updare 3.7.7

---
+++ Shop Site/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Site.Data;
+
+namespace Shop_Site.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public AdminController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult AdminPage()
+        {
+            return View(context.Products.ToList());
+        }
+
+    }
+}
